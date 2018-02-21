657bfd91a22d80cbfd448b21697c047efcc1cbfd
chataro0 <chataro0>
2018-02-21 14:36:44 +0900

    first commit

---
+++ main.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+    region = "ap-northeast-01"
+}
+
+resource "aws_instance" "ec2" {
+    ami  = "ami-c2680fa4"      # Amazon Linux 2 LTS Candidate AMI 2017.12.0 (HVM), SSD Volume Type
+    instance_type = "t2.micro"
+}
